81d936216add3499c4dbb2c86c9984c4b219f448
Coby Randquist <randquistcp@gmail.com>
2012-08-08 22:19:26 -0700

    updated submit_to_youtube

--- lib/confreaks/encoder.rb
+++ lib/confreaks/encoder.rb
@@ -3,6 +3,37 @@ module Confreaks
   # this class consists of the logic to submit a job to ZenCoder and Youtube
   # it is a utility class, and instances should not be created.
   class Encoder
+    def self.submit_to_youtube asset
+      @asset = asset
+
+      creds = "--email=coby@confreaks.com --password=c0nFr34k5"
+
+      title = %&--title="#{@asset.video.title}"&
+      category = %&--category="Tech"&
+      description = ""
+      # description = %&--description="#{@asset.video.abstract}"&
+      keywords = %&--keywords="#{@asset.video.event.short_code}"&
+
+      video_info = [title,category].join(" ")
+
+      file = RAILS_ROOT + "/../../shared" + @asset.data.url.split("?")[0]
+
+      puts video_info
+      puts file
+
+      command = "youtube-upload #{creds} #{video_info} #{file}"
+
+      puts "Command: #{command}"
+
+      results = `#{command}`
+
+      youtube_code = results.split("=")[1]
+
+      @asset.video.youtube_code = youtube_code
+      @asset.video.save
+
+    end
+
     def self.submit_to_zencoder asset, size=nil
       @asset = asset
 
@@ -46,7 +77,6 @@ module Confreaks
       @asset.save
 
       return @json_data, @response
-
     end
   end
 end
