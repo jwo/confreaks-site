0607d73b63b7381975b69cb11366ad844ecba8ed
Coby Randquist <randquistcp@gmail.com>
2012-08-18 23:00:26 -0700

    making attach a file do it all...

--- app/controllers/admin/assets_controller.rb
+++ app/controllers/admin/assets_controller.rb
@@ -59,6 +59,17 @@ class Admin::AssetsController < Admin::Controller
       @asset.save
 
       flash[:success]="File: #{full_file} was attached to #{@video.title}"
+
+      @results, @response = Confreaks::Encoder.submit_to_zencoder(@asset)
+
+      flash[:success] += "<br>Video submitted to Zencoder for encoding."
+
+      if Confreaks::Encoder.submit_to_youtube @asset
+        flash[:success] += "<br>Video submitted to Youtube."
+      else
+        flash[:error] = "Video was not successfully submitted to youtube."
+      end
+
     else
       flash[:error] = "File: #{full_file} does not exist"
     end
